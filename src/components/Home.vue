14fe812de295df7ccd0d002fbd697cbc9a75d032
LHolmberg <lukazlovicc@hotmail.com>
2022-02-05 00:34:21 +0100

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,7 @@
 <template>
-	<div class="custom-shape-divider-top-1644017275">
-		<svg
-			data-name="Layer 1"
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 1200 120"
-			preserveAspectRatio="none"
-		>
-			<path
-				d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-				class="shape-fill"
-			></path>
-		</svg>
-	</div>
+	<body>
+		<h1>Test</h1>
+	</body>
 </template>
 
 <script>
@@ -28,24 +18,8 @@ export default {
 };
 </script>
 
-<style scoped>
-.custom-shape-divider-top-1644017275 {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	overflow: hidden;
-	line-height: 0;
-}
-
-.custom-shape-divider-top-1644017275 svg {
-	position: relative;
-	display: block;
-	width: calc(100% + 1.3px);
-	height: 150px;
-}
-
-.custom-shape-divider-top-1644017275 .shape-fill {
-	fill: #ffffff;
+<style>
+body {
+	background-image: linear-gradient(15deg, #13547a 0%, #80d0c7 100%);
 }
 </style>
