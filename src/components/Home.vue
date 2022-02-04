30c45311f53ca4fe2fb217f866c939c5ad3ecd7e
LHolmberg <lukazlovicc@hotmail.com>
2022-02-04 23:35:28 +0100

    test

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,13 @@
 <template>
-	<h1>{{ msg }}</h1>
+	<blog-post title="My journey with Vue"></blog-post>
 </template>
 
 <script>
+const Vue = window.vue;
+Vue.component("blog-post", {
+	props: ["title"],
+	template: "<h3>{{ title }}</h3>",
+});
 export default {
 	name: "Home",
 	props: {
